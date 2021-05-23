bb55268fcb1f89f979cf9b86f4f8190b4684c1b4
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-23 17:23:26 +0000

    Add styling to the login form and page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,39 +1,58 @@
 <template>
-  <div id="login">
-    <h1 class="">login</h1>
-    <form @submit.prevent>
-      <label for="email1">Email</label> <br />
-      <input
-        type="email"
-        name="email"
-        id="email1"
-        placeholder="youremail@domain.com"
-        v-model.trim="loginForm.email"
-        class="input"
-      />
-      <br />
+  <div id="login" class="flex justify-around">
+    <form @submit.prevent class="bg-gray-300 p-8 shadow-2xl mt-16 lg:w-1/4">
+      <!-- <h1 class="text-5xl font-mono font-extrabold text-black py-3 text-center">
+        LOGIN
+      </h1> -->
+      <div class="flex justify-around">
+        <img
+          class="rounded-full shadow-2xl"
+          src="../assets/avatar4.png"
+          alt=""
+          style="height: 80px; width: 80px"
+        />
+      </div>
 
-      <label for="password1">Password</label> <br />
-      <input
-        type="password"
-        name="password"
-        id="password1"
-        placeholder="***********"
-        v-model.trim="loginForm.password"
-        class="input"
-      />
-      <br />
+      <div class="mt-2">
+        <label for="email1" class="form-label">Email</label> <br />
+        <input
+          type="email"
+          name="email"
+          id="email1"
+          placeholder="youremail@domain.com"
+          v-model.trim="loginForm.email"
+          class="input"
+        />
+      </div>
 
-      <button
-        class="bg-purple-900 font-bold text-white text-xl p-3"
-        type="submit"
-        @click="login()"
-      >
+      <div class="mt-3 mb-4">
+        <label for="password1" class="form-label">Password</label> <br />
+        <input
+          type="password"
+          name="password"
+          id="password1"
+          placeholder="***********"
+          v-model.trim="loginForm.password"
+          class="input"
+        />
+      </div>
+
+      <button class="btn-success w-full mb-5" type="submit" @click="login()">
         Login
       </button>
-      <div>
-        <a href="">Forgot password</a> |
-        <a href="">Create account</a>
+
+      <div class="flex justify-between text-center">
+        <span class="text-blue-600 font-medium text-lg"
+          ><router-link :to="{ name: 'Home' }"
+            >forgot password
+          </router-link></span
+        >
+        |
+        <span class="text-blue-600 font-medium text-lg"
+          ><router-link :to="{ name: 'Signup' }"
+            >create account
+          </router-link></span
+        >
       </div>
     </form>
   </div>
