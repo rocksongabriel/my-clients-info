9fa721335132c09f835b7e1a1d0f50004f48296c
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-26 13:52:07 +0000

    Add clients info card component

---
+++ src/components/dashboard/ClientInfoCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="space-y-6">
+    <div v-for="client in clients_info" :key="client.key">
+      <ClientCard :client="client" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ClientCard from "../dashboard/ClientCard";
+
+export default {
+  name: "ClientInfoCard",
+  data() {
+    return {
+      clients_info: [
+        {
+          id: 1,
+          "Company Name": "Amazon Incorporation",
+          "Owner Name": "Jeff Bezoz",
+          "Contact Name": "CEO of Amazon",
+          "Contact Email": "ceo@amazon.com",
+          "Contact Phone": "+23355234882",
+          "Contact Website": "http://www.amazon.com",
+          "Problem of Company": "Bad looking website",
+          "Source of Info": "LinkedIN",
+          "Link to Source": "http://linkedin.com/amazon.com",
+          Address: "Sans Francisco - Amazon",
+        },
+        {
+          id: 2,
+          "Company Name": "Google",
+          "Owner Name": "Sergey Brin",
+          "Contact Name": "Founder of Google",
+          "Contact Email": "ceo@amazon.com",
+          "Contact Phone": "+23355234882",
+          "Contact Website": "http://www.amazon.com",
+          "Problem of Company": "Bad looking website",
+          "Source of Info": "LinkedIN",
+          "Link to Source": "http://linkedin.com/amazon.com",
+          Address: "Sans Francisco - Amazon",
+        },
+        {
+          id: 3,
+          "Company Name": "Facebook",
+          "Owner Name": "Mark Zuckerberg",
+          "Contact Name": "Founder of Google",
+          "Contact Email": "ceo@amazon.com",
+          "Contact Phone": "+23355234882",
+          "Contact Website": "http://www.amazon.com",
+          "Problem of Company": "Bad looking website",
+          "Source of Info": "LinkedIN",
+          "Link to Source": "http://linkedin.com/amazon.com",
+          Address: "Sans Francisco - Amazon",
+        },
+      ],
+    };
+  },
+  props: ["client"],
+  components: {
+    ClientCard,
+  },
+};
+</script>
